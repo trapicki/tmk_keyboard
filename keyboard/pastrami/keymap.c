ddf99c53b8d4219c2f04d5d1c23fc948bf969bce
Patrick Strasser <patrick@wirklich.priv.at>
2016-06-05 00:13:44 +0200

    Added 2x2 keymap (WASD) and mapping debug code.

--- keyboard/pastrami/keymap.c
+++ keyboard/pastrami/keymap.c
@@ -1,18 +1,18 @@
 /*
-Copyright 2012,2013 Jun Wako <wakojun@gmail.com>
+  Copyright 2012,2013 Jun Wako <wakojun@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <stdint.h>
 #include <stdbool.h>
@@ -28,7 +28,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 static const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    {{KC_A}},
+    {{KC_A, KC_W},
+     {KC_S, KC_D}},
 };
 
 /*
@@ -45,17 +46,25 @@ static const uint16_t PROGMEM fn_actions[] = {
 /* translates key to keycode */
 uint8_t keymap_key_to_keycode(uint8_t layer, keypos_t key)
 {
-    if (layer < KEYMAPS_SIZE) {
-        return pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
-    } else {
+    uint8_t keycode;
+
+    if (layer >= KEYMAPS_SIZE) {
         // fall back to layer 0
-        return pgm_read_byte(&keymaps[0][(key.row)][(key.col)]);
+        keycode = pgm_read_byte(&keymaps[0][(key.row)][(key.col)]);
+        xprintf("%s: layer %d >= KEYMAPS_SIZE %d, falling back to layer 0!\n", __FUNCTION__, layer, KEYMAPS_SIZE);
     }
+    else {
+        keycode = pgm_read_byte(&keymaps[(layer)][(key.row)][(key.col)]);
+    }
+    xprintf("%s: layer %02d key 0X%X/0X%X => 0X%X\n", __FUNCTION__, layer, key.row, key.col, keycode);
+
+    return keycode;
 }
 
 /* translates Fn keycode to action */
 action_t keymap_fn_to_action(uint8_t keycode)
 {
+    xprintf("keymap_fn_to_action: keycode x%X\n", keycode);
     action_t action;
     if (FN_INDEX(keycode) < FN_ACTIONS_SIZE) {
         action.code = pgm_read_word(&fn_actions[FN_INDEX(keycode)]);
@@ -64,3 +73,4 @@ action_t keymap_fn_to_action(uint8_t keycode)
     }
     return action;
 }
+
