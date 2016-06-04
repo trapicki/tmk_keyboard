f41a7d852d757a5518aef1bfe3a06a1e3fba8232
Patrick Strasser <patrick@wirklich.priv.at>
2016-06-04 23:54:14 +0200

    Added structure to centrally define pins and ports for each row/col, and how to set them. 2x2 config.

--- keyboard/pastrami/config.h
+++ keyboard/pastrami/config.h
@@ -22,14 +22,14 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* USB Device descriptor parameter */
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0xBEEF
-#define DEVICE_VER      0x0001
+#define DEVICE_VER      0x0002
 #define MANUFACTURER    trapicki
 #define PRODUCT         pastrami
 #define DESCRIPTION     t.m.k. keyboard firmware for pastrami keyboard
 
 /* key matrix size */
-#define MATRIX_ROWS 1
-#define MATRIX_COLS 1
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 2
 
 /* define if matrix has ghost */
 //#define MATRIX_HAS_GHOST
--- keyboard/pastrami/matrix.c
+++ keyboard/pastrami/matrix.c
@@ -1,18 +1,18 @@
 /*
-Copyright 2012 Jun Wako <wakojun@gmail.com>
+  Copyright 2012 Jun Wako <wakojun@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 /*
@@ -32,15 +32,69 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #   define DEBOUNCE	5
 #endif
 
-#define DDR_ROW DDRF
-#define PORT_ROW PORTF
-#define PIN_ROW PINF
-#define BIT_ROW 6
+/// comment/uncomment according to number of MATRIX_ROWS and MATRIX_COLS in config.h
+#define PORT_COL_0 F
+#define BIT_COL_0 3
+#define PORT_COL_1 F
+#define BIT_COL_1 4
+#define PORT_COL_2 F
+#define BIT_COL_2 4
+#define PORT_COL_3 F
+#define BIT_COL_3 4
+#define PORT_COL_4 F
+#define BIT_COL_4 4
+#define PORT_COL_5 F
+#define BIT_COL_5 4
+#define PORT_COL_6 F
+#define BIT_COL_6 4
+#define PORT_COL_7 F
+#define BIT_COL_7 4
+#define PORT_COL_8 F
+#define BIT_COL_8 4
+#define PORT_COL_9 F
+#define BIT_COL_9 4
+#define PORT_COL_10 F
+#define BIT_COL_10 4
+#define PORT_COL_11 F
+#define BIT_COL_11 4
+#define PORT_COL_12 F
+#define BIT_COL_12 4
+#define PORT_COL_13 F
+#define BIT_COL_13 4
+#define PORT_COL_14 F
+#define BIT_COL_14 4
+#define PORT_COL_15 F
+#define BIT_COL_15 4
 
-#define DDR_COL DDRF
-#define PORT_COL PORTF
-#define PIN_COL PINF
-#define BIT_COL 7
+#define PORT_ROW_0 F
+#define BIT_ROW_0 6
+#define PORT_ROW_1 F
+#define BIT_ROW_1 7
+#define PORT_ROW_2 F
+#define BIT_ROW_2 7
+#define PORT_ROW_3 F
+#define BIT_ROW_3 7
+#define PORT_ROW_4 F
+#define BIT_ROW_4 7
+#define PORT_ROW_5 F
+#define BIT_ROW_5 7
+#define PORT_ROW_6 F
+#define BIT_ROW_6 7
+#define PORT_ROW_7 F
+#define BIT_ROW_7 7
+///
+
+
+// some preprocessort tricks:
+// CAT concatenates
+// CAT_INDIRECT introduces additional indirection, giving thre preprocessor one more turn to resolve macros.
+#define CAT(a, b) a ## b
+#define CAT_INDIRECT(a, b) CAT(a, b)
+
+// define the right register
+#define DDR(a) CAT_INDIRECT(DDR, PORT_##a)
+#define PIN(a) CAT_INDIRECT(PIN, PORT_##a)
+#define PORT(a) CAT_INDIRECT(PORT, PORT_##a)
 
 #define MATRIX_PRINT_HEADER_INTERVAL 20
 
@@ -145,7 +199,7 @@ void matrix_print(void)
 }
 
 
- uint8_t matrix_key_count(void)
+uint8_t matrix_key_count(void)
 {
     uint8_t count = 0;
     for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
@@ -154,40 +208,103 @@ void matrix_print(void)
     return count;
 }
 
-/* Column pin configuration
- * col: 0
- * pin: B0
- */
 static void  init_cols(void)
 {
-    // Input with pull-up(DDR:0, PORT:1)
-    DDR_COL  &= ~(1<<BIT_COL);
-    PORT_COL |=  (1<<BIT_COL);
+// Input with pull-up(DDR:0, PORT:1)
+#define INIT_COL(a)  DDR(COL_##a)  &= ~(1<<BIT_COL_##a);\
+                     PORT(COL_##a) |=  (1<<BIT_COL_##a)
+    /// comment/uncomment according to number of MATRIX_ROWS and MATRIX_COLS in config.h
+    INIT_COL(0);
+    INIT_COL(1);
+    /*
+    INIT_COL(2);
+    INIT_COL(3);
+    INIT_COL(4);
+    INIT_COL(5);
+    INIT_COL(6);
+    INIT_COL(7);
+    INIT_COL(8);
+    INIT_COL(9);
+    INIT_COL(10);
+    INIT_COL(11);
+    INIT_COL(12);
+    INIT_COL(13);
+    INIT_COL(14);
+    INIT_COL(15);
+    */
+    ///
+#undef INITCOL
 }
 
 static matrix_row_t read_cols(void)
 {
-    return (PIN_COL & (1<<BIT_COL) ? 0 : (1<<0));
+    //
+#define ADD_COL(col) (PIN(COL_##col) & (1<<BIT_COL_##col) ? 0 : (1<<col))
+    return (
+            /// comment/uncomment according to number of MATRIX_ROWS and MATRIX_COLS in config.h
+            ADD_COL(0)
+          | ADD_COL(1)
+            /*
+          | ADD_COL(2)
+          | ADD_COL(3)
+          | ADD_COL(4)
+          | ADD_COL(5)
+          | ADD_COL(6)
+          | ADD_COL(7)
+          | ADD_COL(8)
+          | ADD_COL(9)
+          | ADD_COL(10)
+          | ADD_COL(11)
+          | ADD_COL(12)
+          | ADD_COL(13)
+          | ADD_COL(14)
+          | ADD_COL(15)
+            */
+            ///
+     );
+#undef ADD_COL
 }
 
-/* Row pin configuration
- * row: 0
- * pin: B1
- */
 static void unselect_rows(void)
 {
-    // Hi-Z(DDR:0, PORT:0) to unselect
-    DDR_ROW  &= ~(1<<BIT_ROW);
-    PORT_ROW &= ~(1<<BIT_ROW);
+// Hi-Z(DDR:0, PORT:0) to unselect
+#define UNSELECT_ROW(row) DDR(ROW_##row &= ~(1<<BIT_ROW_##row)); \
+                     PORT(ROW_##row &= ~(1<<BIT_ROW_##row))
+    /// comment/uncomment according to number of MATRIX_ROWS and MATRIX_COLS in config.h
+    UNSELECT_ROW(0);
+    UNSELECT_ROW(1);
+    /*
+    UNSELECT_ROW(2);
+    UNSELECT_ROW(3);
+    UNSELECT_ROW(4);
+    UNSELECT_ROW(5);
+    UNSELECT_ROW(6);
+    UNSELECT_ROW(7);
+    */
+    ///
+#undef UNSELECT_ROW
 }
 
 static void select_row(uint8_t row)
 {
-    // Output low(DDR:1, PORT:0) to select
+// Output low(DDR:1, PORT:0) to select
+#define ADD_CASE(row) case row: \
+    DDR(ROW_##row)  |=  (1<<BIT_ROW_##row);           \
+    PORT(ROW_##row) &= ~(1<<BIT_ROW_##row);           \
+    break
     switch (row) {
-        case 0:
-            DDR_ROW  |=  (1<<BIT_ROW);
-            PORT_ROW &= ~(1<<BIT_ROW);
-            break;
+        /// comment/uncomment according to number of MATRIX_ROWS and MATRIX_COLS in config.h
+        ADD_CASE(0);
+        ADD_CASE(1);
+        /*
+        ADD_CASE(2);
+        ADD_CASE(3);
+        ADD_CASE(4);
+        ADD_CASE(5);
+        ADD_CASE(6);
+        ADD_CASE(7);
+        */
+        ///
     }
+#undef ADD_CASE
 }
